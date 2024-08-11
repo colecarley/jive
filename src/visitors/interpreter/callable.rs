e5864aba6a631ef0ae8e3e03685919c1b3a2852f
Cole Carley <cole.carley@gmail.com>
2024-08-11 14:30:37 -0700

    cleanup

--- src/visitors/interpreter/callable.rs
+++ src/visitors/interpreter/callable.rs
@@ -76,8 +76,8 @@ pub fn println(_interpreter: &mut Interpreter, arguments: Vec<Value>) -> Value {
             Value::Number(number) => print!("{}", number),
             Value::Boolean(boolean) => print!("{}", boolean),
             Value::String(string) => print!("{}", string),
-            Value::BuiltIn(callable) => print!("<native funk>"),
-            Value::Function(function) => print!("<funk>"),
+            Value::BuiltIn(_) => print!("<native funk>"),
+            Value::Function(_) => print!("<funk>"),
             Value::Iter(iter) => print!(
                 "Iter [{}]",
                 iter.iter()
